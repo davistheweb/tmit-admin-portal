97c80d4f03dbcb3da0ba731aa7134ac956468b8e
davistheweb <josiahdave001@gmail.com>
2025-07-29 15:22:49 +0100

    upated app route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { Login } from "./auth/pages";
 import { Dashboard } from "@/pages/dashboard/Dashboard";
 import { Index } from "@/pages/dashboard/Index";
-// import { Applications } from "@/pages/dashboard/Applications";
+import { Academics } from "@/pages/dashboard/Academics";
+import { ManageFaculty } from "@/pages/dashboard/ManageFaculty";
 import Fees from "@/pages/dashboard/Fees";
 import ChangePassword from "@/pages/dashboard/ChangePassword";
 import IndexPage from "./IndexPage";
 import { ProtectedRoute } from "./routes/ProtectedRoutes";
 import { DepartmentSelector } from "./pages/dashboard/DepartmentSelector";
 import { ApplicationsPage } from "./pages/dashboard/ApplicationsPage";
-import { Setup } from "@/pages/dashboard/Setup";
 
 const App: React.FC = () => {
   return (
@@ -30,13 +30,14 @@ const App: React.FC = () => {
             }
           >
             <Route index element={<Index />} />
+            <Route path="academics" element={<Academics />} />
             <Route
-              path="/dashboard/select-department"
-              element={<DepartmentSelector />}
+              path="academics/manage-faculty"
+              element={<ManageFaculty />}
             />
+            <Route path="select-department" element={<DepartmentSelector />} />
             <Route path="applications" element={<ApplicationsPage />} />
             <Route path="fees" element={<Fees />} />
-            <Route path="setup" element={<Setup />} />
             <Route path="change-password" element={<ChangePassword />} />
           </Route>
           <Route path="*" element={<p>Not found!</p>} />
