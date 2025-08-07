ef884cb2f8998ba47a07689291634293f1989326
davistheweb <josiahdave001@gmail.com>
2025-08-07 15:54:51 +0100

    added indedx to pages/dashboard for clean import and export

---
+++ src/pages/dashboard/index.ts
@@ -0,0 +1,23 @@
+import { Academics } from "./Academics";
+import { AddResults } from "./AddResults";
+import { ApplicationsPage } from "./ApplicationsPage";
+import { ChangePassword } from "./ChangePassword";
+import { Dashboard } from "./Dashboard";
+import { DashboardIndex } from "./DashboardIndex";
+import { DepartmentSelector } from "./DepartmentSelector";
+import Fees from "./Fees";
+import { ManageDepartment } from "./ManageDepartment";
+import { ManageFaculty } from "./ManageFaculty";
+
+export {
+  Academics,
+  AddResults,
+  ApplicationsPage,
+  ChangePassword,
+  Dashboard,
+  DashboardIndex,
+  DepartmentSelector,
+  Fees,
+  ManageDepartment,
+  ManageFaculty,
+};
