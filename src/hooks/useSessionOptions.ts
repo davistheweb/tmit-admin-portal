e610426583b27f375230b169321600f2b5bd9610
davistheweb <josiahdave001@gmail.com>
2025-08-07 15:33:23 +0100

    code format

--- src/hooks/useSessionOptions.ts
+++ src/hooks/useSessionOptions.ts
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
-
- // this code Generates session options (current year to next 5 years, which will be extensible)
- // I used this so that in the next 5 years, it will be updated
+// this code Generates session options (current year to next 5 years, which will be extensible)
+// I used this so that in the next 5 years, it will be updated
 export const useSessionOptions = (setSessionValue: CallableFunction) => {
   const currentYear = new Date().getFullYear();
   const initialSessions = Array.from({ length: 6 }, (_, i) => {
@@ -27,19 +26,20 @@ export const useSessionOptions = (setSessionValue: CallableFunction) => {
   const handleSessionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = e.target.value;
     const selectedYear = parseInt(`20${selectedValue}`);
-   if (selectedYear >= currentYear + 4) {
-     const nextSessions = Array.from({ length: 5 }, (_, i) => {
-       const year = selectedYear + 1 + i;
-       return {
-         label: `${year}/${year + 1}`,
-         value: (year % 100).toString().padStart(2, "0"),
-       };
-     }).filter(
-       (newSession) => !sessionOptions.some((s) => s.value === newSession.value)
-     );
+    if (selectedYear >= currentYear + 4) {
+      const nextSessions = Array.from({ length: 5 }, (_, i) => {
+        const year = selectedYear + 1 + i;
+        return {
+          label: `${year}/${year + 1}`,
+          value: (year % 100).toString().padStart(2, "0"),
+        };
+      }).filter(
+        (newSession) =>
+          !sessionOptions.some((s) => s.value === newSession.value),
+      );
 
-     setSessionOptions((prev) => [...prev, ...nextSessions]);
-   }
+      setSessionOptions((prev) => [...prev, ...nextSessions]);
+    }
 
     setSessionValue("session", selectedValue);
   };
